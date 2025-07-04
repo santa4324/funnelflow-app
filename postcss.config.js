d7c46327fe2428c729aae407852e20a7531aa78b
Bump Haven Store <bumphavenstore@gmail.com>
2025-07-04 17:31:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-/**
- * This file is intentionally left blank to resolve build issues.
- * Configuration will be reintroduced later.
-*/
-module.exports = {};
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
