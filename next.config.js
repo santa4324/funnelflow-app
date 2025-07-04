47baccf1f29d44723a26937346c48f3a8881eb4c
Bump Haven Store <bumphavenstore@gmail.com>
2025-07-04 10:13:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
