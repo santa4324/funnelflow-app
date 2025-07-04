bf8b7c33f52729d9024de0d2e50401b1763ebf73
Bump Haven Store <bumphavenstore@gmail.com>
2025-07-04 09:54:32 +0000

    theres another error "Failed: a1c85b87-302f-48c3-be5b-f19b7b1e75e1"

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // This is the fallback for Webpack (used in production builds).
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
